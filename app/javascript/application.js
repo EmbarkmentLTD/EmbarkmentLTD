a0effd49b597db561fbe0bf57d9a385dd8145efa
KvngJamesNG <adesinaadewale28@yahoo.com>
2026-02-08 00:59:51 +0000

    Removed the missing devise/turbo import that was breaking all JS modules. See application.js.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "devise/turbo"
 import "controllers"
 import Chart from "chart.js/auto"
 
