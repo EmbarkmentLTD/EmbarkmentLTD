3f68e95a662c20ff58da764ca885a33781a4ddc5
KvngJamesNG <adesinaadewale28@yahoo.com>
2026-09-19 01:39:49 +0100

    fix: allow password login when resend key is unavailable

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,11 +11,15 @@ class Users::SessionsController < Devise::SessionsController
     self.resource = resource_class.find_for_database_authentication(email: email)
 
     if resource.present? && resource.valid_password?(password)
-      session[:pending_sign_in_id] = resource.id
       sign_in(resource_name, resource)
 
       set_flash_message!(:notice, :signed_in) if is_flashing_format?
-      respond_with resource, location: sign_in_verification_path
+      if ENV["RESEND_API_KEY"].present?
+        session[:pending_sign_in_id] = resource.id
+        respond_with resource, location: sign_in_verification_path
+      else
+        respond_with resource, location: products_path
+      end
       return
     end
 
@@ -28,7 +32,7 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(resource)
-    sign_in_verification_path
+    ENV["RESEND_API_KEY"].present? ? sign_in_verification_path : products_path
   end
 
   def after_sign_out_path_for(resource)
